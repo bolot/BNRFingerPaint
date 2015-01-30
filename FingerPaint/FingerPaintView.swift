e425307ac0aa6a7431affa5a46975e2b59b6fa37
Bolot Kerimbaev <bolot@bignerdranch.com>
2015-01-30 10:46:30 -0800

    add FingerPaintView

---
+++ FingerPaint/FingerPaintView.swift
@@ -0,0 +1,21 @@
+//
+//  FingerPaintView.swift
+//  FingerPaint
+//
+//  Created by Bolot Kerimbaev on 1/30/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class FingerPaintView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
