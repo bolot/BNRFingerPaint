c1b128f52e5b40c2661467060daf65787bf65869
Bolot Kerimbaev <bolot@bignerdranch.com>
2015-01-30 10:49:37 -0800

    add touch tracking and drawing using UIBezierPath

--- FingerPaint/FingerPaintView.swift
+++ FingerPaint/FingerPaintView.swift
@@ -9,13 +9,50 @@
 import UIKit
 
 class FingerPaintView: UIView {
+    var paths = [UIBezierPath]()
+    var pathsInProgress = [UITouch:UIBezierPath]()
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect) {
         // Drawing code
+        UIColor.blackColor().setStroke()
+        for path in paths {
+            path.stroke()
+        }
+        UIColor.redColor().setStroke()
+        for (touch, path) in pathsInProgress {
+            path.stroke()
+        }
     }
-    */
-
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        for touch in touches.allObjects as [UITouch] {
+            let path = UIBezierPath()
+            path.moveToPoint(touch.locationInView(self))
+            pathsInProgress[touch] = path
+        }
+    }
+    
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        for touch in touches.allObjects as [UITouch] {
+            let path = pathsInProgress[touch]
+            path?.addLineToPoint(touch.locationInView(self))
+        }
+        self.setNeedsDisplay()
+    }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        for touch in touches.allObjects as [UITouch] {
+            if let path = pathsInProgress.removeValueForKey(touch) {
+                paths.append(path)
+            }
+        }
+        self.setNeedsDisplay()
+    }
+    
+    override func touchesCancelled(touches: NSSet!, withEvent event: UIEvent!) {
+        for touch in touches.allObjects as [UITouch] {
+            pathsInProgress.removeValueForKey(touch)
+        }
+        self.setNeedsDisplay()
+    }
 }
